c9eed94d0eadf4c603b2d7aea926653cf5c0aeec
budimkic <budimkic@proton.me>
2026-07-28 10:01:48 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+
+        val count = HashMap<Char, Int>()
+
+        for (i in s.indices) {
+            count[s[i]] = (count[s[i]] ?: 0) + 1
+            count[t[i]] = (count[t[i]] ?: 0) - 1
+        }
+        return count.values.all { it == 0 }
+    }
+}
