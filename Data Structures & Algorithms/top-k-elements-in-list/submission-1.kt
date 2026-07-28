6c7e2087038e3c53d45915997f266f2a91142400
budimkic <budimkic@proton.me>
2026-07-28 10:47:52 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+
+        for (num in nums) {
+            map[num] = (map[num] ?: 0) + 1
+        }
+        return map.entries
+                  .sortedByDescending { it.value }
+                  .take(k)
+                  .map { it.key}
+                  .toIntArray()
+
+    }
+}
